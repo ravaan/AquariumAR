6c8a3a6723eccd4f1b8420e890acc2a5f49a18da
ravaan <arpit.agarwal181@gmail.com>
2017-09-15 09:05:51 -0700

    added description  dialog

---
+++ Assets/Scripts/PanelScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelScript : MonoBehaviour {
+    public GameObject description;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowDescription()
+    {
+        //GameObject.Find("Description_Big").GetComponent().enabled = true;
+        //GameObject.FindWithTag("Description").SetActive(true);
+        print("hey");
+        description.SetActive(true);
+        StartCoroutine(timer());
+
+    }
+    
+    IEnumerator timer()
+    {
+        yield return new WaitForSeconds(5);
+
+        //GameObject.Find("Description_Big").GetComponent(Renderer).enabled = false;
+        //GameObject.FindWithTag("Description").SetActive(false);
+        description.SetActive(false);
+    }
+}
